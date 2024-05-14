1edd24b55d7188b29490615f6d52dc630e3cfc04
Pondpdpr <pBoonnud@gmail.com>
2024-05-14 15:36:54 +0700

    add migration to add password user

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -14,6 +14,9 @@ export class User {
   name: string;
 
   @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ type: 'varchar' })
   email: string;
 
   @OneToMany(() => Reservation, (reservation) => reservation.user)
